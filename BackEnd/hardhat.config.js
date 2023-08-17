b1890d12061da2eff15956d4fe26a5943eb05600
MohammadRokib <rokibk548@gmail.com>
2023-08-17 10:32:21 +0600

    commented test info

--- BackEnd/hardhat.config.js
+++ BackEnd/hardhat.config.js
@@ -6,11 +6,11 @@ require("hardhat-gas-reporter");
 require("hardhat-contract-sizer");
 require("dotenv").config();
 
-GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli";
-SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia";
-PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
-ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
-COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
+// GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli";
+// SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia";
+// PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+// ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
+// COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -25,19 +25,19 @@ module.exports = {
             chainId: 31337,
             blockConfirmations: 1,
         },
-        sepolia: {
-            chainId: 11155111,
-            blockConfirmations: 1,
-            url: SEPOLIA_RPC_URL,
-            accounts: [PRIVATE_KEY],
-        },
+        // sepolia: {
+        //     chainId: 11155111,
+        //     blockConfirmations: 1,
+        //     url: SEPOLIA_RPC_URL,
+        //     accounts: [PRIVATE_KEY],
+        // },
     },
     gasReporter: {
         enabled: false,
         currency: "USD",
         outputFile: "gas-report.txt",
         noColors: true,
-        coinmarketcap: COINMARKETCAP_API_KEY,
+        // coinmarketcap: COINMARKETCAP_API_KEY,
     },
     contractSizer: {
         runOnCompile: false,
@@ -51,9 +51,9 @@ module.exports = {
             default: 1,
         },
     },
-    etherscan: {
-        apiKey: process.env.ETHERSCAN_API_KEY,
-    },
+    // etherscan: {
+    //     apiKey: process.env.ETHERSCAN_API_KEY,
+    // },
     mocha: {
         timeout: 500000,
     }
